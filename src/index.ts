db220f0b0d61aba28b368e5fc39ab47309c702b9
Robert J Beall <rbeall@smcm.edu>
2025-05-07 15:50:10 -0400

    Add test route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express, { Application } from 'express';
 import 'dotenv/config';
+import helloRouter from './routes/hello';
 
 const app: Application = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.use(helloRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Gibberish API running on http://localhost:${process.env.PORT}`);
