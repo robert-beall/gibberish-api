be34808c55baddb911dc033ef071b83da2779bb3
Robert J Beall <rbeall@smcm.edu>
2025-05-07 15:22:14 -0400

    Add basic web server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(5);
+import express, { Application } from 'express';
+import 'dotenv/config';
+
+const app: Application = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Gibberish API running on http://localhost:${process.env.PORT}`);
+});
