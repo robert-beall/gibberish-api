52f70412c59a7cacad58535b3e90f1a939ea5024
Robert J Beall <rbeall@smcm.edu>
2025-05-09 14:29:25 -0400

    add initial tests

--- src/routes/info.test.ts
+++ src/routes/info.test.ts
@@ -2,22 +2,38 @@ import request from 'supertest';
 import express from 'express';
 import infoRouter from './info';
 import crypto from 'crypto';
-import {jest} from '@jest/globals'; 
 
-const app = express();
-app.use('/info/', infoRouter);
 const uriPrefix = '/info';
+const app = express();
+
+beforeAll(() => {
+    app.use('/info/', infoRouter);
+    jest.mock('crypto');
+});
+
+beforeEach(() => {
+    jest.clearAllMocks();
+})
 
 describe('get-ciphers', () => {
-    jest.mock('crypto');
-
     test('SUCCESS', async () => {
-        // Cast to jest.Mock to enable mock methods
-        const getCiphersMock = crypto.getCiphers as jest.Mock<string[], []>;
-        getCiphersMock.mockReturnValue(['hello', 'world']);
-
+        crypto.getCiphers = jest.fn();
+        (crypto.getCiphers as jest.Mock).mockReturnValue(['hello', 'world']);
         const response = await request(app)
         .get(`${uriPrefix}/get-ciphers`)
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+        expect(response.body).toBeDefined();
+    });
+});
+
+describe('get-hashes', () => {
+    test('SUCCESS', async () => {
+        crypto.getHashes = jest.fn();
+        (crypto.getHashes as jest.Mock).mockReturnValue(['hello', 'world']);
+        const response = await request(app)
+        .get(`${uriPrefix}/get-hashes`)
         .expect("Content-Type", /json/)
         .expect(200);
 
